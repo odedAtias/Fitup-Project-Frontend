862503540e9447bec7b1263c9a8451653ba031c9
Oded <odedatias8115@gmail.com>
2023-02-17 16:53:33 +0200

    Adding Screens files

---
+++ Screens/Login.js
@@ -0,0 +1,16 @@
+// RN core components & API imports
+import { View, Text, StyleSheet } from 'react-native';
+
+// Login Component
+const Login = () => {
+	return (
+		<View>
+			<Text>Login</Text>
+		</View>
+	);
+};
+
+// Login StyleSheet
+const styles = StyleSheet.create({});
+
+export default Login;
