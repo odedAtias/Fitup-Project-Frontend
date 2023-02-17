6e7ae4d90f63c9bb9378d8acc2501723fce2caa1
Oded <odedatias8115@gmail.com>
2023-02-17 14:03:09 +0200

    Adding colors pallets #1

---
+++ Constants/Colors.js
@@ -0,0 +1,23 @@
+const Colors = {
+	Headers: {
+		primary: '#9fd3c7',
+		secondary: '#f4f4f4',
+	},
+	Backgrounds: {
+		primary: '#ececec',
+		secondary: '#e4f6f6',
+		male: '#604fdd',
+		female: '#ff78ae',
+	},
+	Buttons: {
+		primary: '#9fd3c7',
+		secondary: '#010101',
+	},
+	Links: {
+		primary: '#2657c1',
+	},
+	Texts: {
+		primary: '#010101',
+		secondary: '#f4f4f4',
+	},
+};
