e0e6769a15733a9205befa4bbc6174c04bacc73c
Oded <odedatias8115@gmail.com>
2023-03-02 20:27:46 +0200

    Fixing natural behavior of the keyboard when opening the keyboad in the signup screen

--- Components/SignupOutput/SignupForm.js
+++ Components/SignupOutput/SignupForm.js
@@ -1,13 +1,15 @@
 // Hooks imports
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-
 // RN core components & API imports
-import { View, StyleSheet } from 'react-native';
-
+import {
+	View,
+	StyleSheet,
+	KeyboardAvoidingView,
+	ScrollView,
+} from 'react-native';
 // Costants
 import Colors from '../../Constants/Colors';
-
 // Custom components imports
 import SignupInput from './SignupInput';
 import Button from '../Button';
@@ -56,70 +58,74 @@ const SignupForm = () => {
 		});
 	};
 	return (
-		<View style={styles.container}>
-			<SignupInput
-				label='First Name'
-				inputConfigurations={{
-					onChangeText: handleInputChange.bind(this, 'firstName'),
+		<ScrollView style={styles.screen}>
+			<KeyboardAvoidingView stlye={styles.screen} behavior='padding'>
+				<View style={styles.container}>
+					<SignupInput
+						label='First Name'
+						inputConfigurations={{
+							onChangeText: handleInputChange.bind(this, 'firstName'),
 
-					autoCorrect: false,
-					value: inputs.firstName.value,
-				}}
-			/>
-			<SignupInput
-				label='Last Name'
-				inputConfigurations={{
-					onChangeText: handleInputChange.bind(this, 'lastName'),
-					autoCorrect: false,
-					value: inputs.lastName.value,
-				}}
-			/>
-			<SignupInput
-				label='Email'
-				inputConfigurations={{
-					onChangeText: handleInputChange.bind(this, 'email'),
-					autoCorrect: false,
-					value: inputs.email.value,
-				}}
-			/>
-			<SignupInput
-				label='Password'
-				inputConfigurations={{
-					onChangeText: handleInputChange.bind(this, 'password'),
-					autoCorrect: false,
-					secureTextEntry: true,
-					value: inputs.password.value,
-				}}
-			/>
-			<SignupInput
-				label='Confirm Password'
-				inputConfigurations={{
-					onChangeText: handleInputChange.bind(this, 'confirmPassword'),
-					autoCorrect: false,
-					secureTextEntry: true,
-					value: inputs.confirmPassword.value,
-				}}
-			/>
-			<SignupInput
-				label='Trainee / Trainer'
-				inputConfigurations={{
-					onChangeText: handleInputChange.bind(this, 'userType'),
-					autoCorrect: false,
-					secureTextEntry: true,
-					value: inputs.userType.value,
-				}}
-			/>
-			<View style={{ justifyContent: 'center', alignItems: 'center' }}>
-				<Button
-					style={{ backgroundColor: Colors.Buttons.primary }}
-					onPress={() => {
-						console.log('sign-up handler');
-						navigation.navigate('Login');
-					}}>
-					Register now
-				</Button>
-			</View>
-		</View>
+							autoCorrect: false,
+							value: inputs.firstName.value,
+						}}
+					/>
+					<SignupInput
+						label='Last Name'
+						inputConfigurations={{
+							onChangeText: handleInputChange.bind(this, 'lastName'),
+							autoCorrect: false,
+							value: inputs.lastName.value,
+						}}
+					/>
+					<SignupInput
+						label='Email'
+						inputConfigurations={{
+							onChangeText: handleInputChange.bind(this, 'email'),
+							autoCorrect: false,
+							value: inputs.email.value,
+						}}
+					/>
+					<SignupInput
+						label='Password'
+						inputConfigurations={{
+							onChangeText: handleInputChange.bind(this, 'password'),
+							autoCorrect: false,
+							secureTextEntry: true,
+							value: inputs.password.value,
+						}}
+					/>
+					<SignupInput
+						label='Confirm Password'
+						inputConfigurations={{
+							onChangeText: handleInputChange.bind(this, 'confirmPassword'),
+							autoCorrect: false,
+							secureTextEntry: true,
+							value: inputs.confirmPassword.value,
+						}}
+					/>
+					<SignupInput
+						label='Trainee / Trainer'
+						inputConfigurations={{
+							onChangeText: handleInputChange.bind(this, 'userType'),
+							autoCorrect: false,
+							secureTextEntry: true,
+							value: inputs.userType.value,
+						}}
+					/>
+					<View style={{ justifyContent: 'center', alignItems: 'center' }}>
+						<Button
+							style={{ backgroundColor: Colors.Buttons.primary }}
+							onPress={() => {
+								console.log('sign-up handler');
+								navigation.navigate('Login');
+							}}>
+							Register now
+						</Button>
+					</View>
+				</View>
+			</KeyboardAvoidingView>
+		</ScrollView>
 	);
 };
 const handleSubmit = () => {
@@ -129,6 +135,9 @@ const handleSubmit = () => {
 const styles = StyleSheet.create({
 	container: {
 		paddingHorizontal: '5%',
+	},
+	screen: {
+		flex: 1,
 	},
 });
 
