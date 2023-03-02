9d686dca96c788925faea8e03899b0a6c1a395a8
Oded <odedatias8115@gmail.com>
2023-03-02 18:44:08 +0200

    Working on Event component

--- Components/EventsOutput/Event.js
+++ Components/EventsOutput/Event.js
@@ -1,17 +1,43 @@
 // RN core components & API imports
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 
 // Event component
 const Event = ({ event }) => {
 	return (
 		// Main container
-		<View>
+		<View style={styles.container}>
 			{/* Events trainer image container */}
-			<View></View>
+			<View>
+				<Image source={event.imageUrl} style={styles.image} />
+			</View>
 			{/* Events details container */}
-			<View></View>
+			<View>
+				<Text>{event.trainerName}</Text>
+				<Text>{event.address}</Text>
+				<View style={{ flexDirection: 'row' }}>
+					<Text style={{ marginRight: 10 }}>{event.date}</Text>
+					<Text>{event.hour}</Text>
+				</View>
+				<Text>{`${event.numOfTrainees}/${event.maxNumOfTrainees} Collaborators`}</Text>
+			</View>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		marginVertical: 20,
+	},
+	// Todo : check the images in defferent devices
+	image: {
+		marginRight: 20,
+		marginLeft: 5,
+		height: 100,
+		width: 100,
+		borderRadius: 80,
+	},
+});
 
 export default Event;
