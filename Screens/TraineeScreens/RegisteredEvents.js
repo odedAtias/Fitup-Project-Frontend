707f9d76daf9c9fddd7284932e4aedacc55b7b2f
Or Dayani <odayani21@gmail.com>
2023-03-03 15:22:44 +0200

    First PersonalDetails and RegisterEvents screens.

--- Screens/TraineeScreens/RegisteredEvents.js
+++ Screens/TraineeScreens/RegisteredEvents.js
@@ -1,13 +1,51 @@
 // RN core components & API imports
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+
+import EventsList from "../../Components/EventsOutput/EventsList";
 
 // RegisteredEvents component
 const RegisteredEvents = () => {
-	return (
-		<View>
-			<Text>RegisteredEvents</Text>
-		</View>
-	);
+  //Dummy RegisteredEvents
+  const EVENTS = [
+    {
+      id: "event1",
+      category: "Spining",
+      trainerName: "Max shultz",
+      date: "14/12/23",
+      hour: "17:00 pm",
+      address: "Hatnofa 18 st",
+      city: "Jerusalem",
+      numOfTrainees: 13,
+      maxNumOfTrainees: 15,
+      imageUrl: require("../../Images/Trainers/trainer1.png"),
+    },
+    {
+      id: "event2",
+      category: "Spining",
+      trainerName: "Max shultz",
+      date: "14/12/23",
+      hour: "17:00 pm",
+      address: "Hatnofa 18 st",
+      city: "Jerusalem",
+      numOfTrainees: 13,
+      maxNumOfTrainees: 15,
+      imageUrl: require("../../Images/Trainers/trainer1.png"),
+    },
+  ];
+  return (
+    <View>
+      <EventsList events={EVENTS} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    fontSize: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 24,
+  },
+});
 
 export default RegisteredEvents;
