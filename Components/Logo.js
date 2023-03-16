7d5a7545fbfca0e99066f4581efa91643faf1f77
Oded <odedatias8115@gmail.com>
2023-03-16 18:46:01 +0200

    Cleaning Logo component

--- Components/Logo.js
+++ Components/Logo.js
@@ -2,17 +2,16 @@
 import { StyleSheet, View, Image } from 'react-native';
 
 //  Logo component
-const Logo = ({ imageWidth, imageHeight }) => {
-	return (
-		<View style={styles.logoContainer}>
-			<Image
-				style={{ width: imageWidth, height: imageHeight }}
-				source={require('../Images/FitUp-Logo.png')}
-			/>
-		</View>
-	);
-};
+const Logo = ({ imageWidth, imageHeight }) => (
+	<View style={styles.logoContainer}>
+		<Image
+			style={{ width: imageWidth, height: imageHeight }}
+			source={require('../Images/FitUp-Logo.png')}
+		/>
+	</View>
+);
 
+// Logo StyleSheet
 const styles = StyleSheet.create({
 	logoContainer: {
 		alignItems: 'center',
