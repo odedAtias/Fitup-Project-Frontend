a10954e04671fa4e87f0b074e76893508bfa89c8
Oded <odedatias8115@gmail.com>
2023-03-27 09:28:23 +0300

    Fixes #9, Fixes #11, Fixes #12, Fixes #14

--- server/models/trainee.js
+++ server/models/trainee.js
@@ -1,56 +1,56 @@
 // Modules imports
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const Trainee = mongoose.model(
-  "Trainee",
-  new mongoose.Schema({
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    registeredEvents: {
-      type: [mongoose.Schema.Types.ObjectId],
-      default: [],
-      ref: "Event",
-    },
-    favoriteTrainers: {
-      type: [mongoose.Schema.Types.ObjectId],
-      default: [],
-      ref: "Trainers",
-    },
-    // TraineeImg: { --> url link to image
-    //   type: String,
-    // },
-  })
+	'Trainee',
+	new mongoose.Schema({
+		firstName: {
+			type: String,
+			min: 2,
+			max: 20,
+			required: true,
+		},
+		lastName: {
+			type: String,
+			min: 2,
+			max: 20,
+			required: true,
+		},
+		email: {
+			type: String,
+			unique: true,
+			match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+			lowercase: true,
+			required: true,
+		},
+		registeredEvents: {
+			type: [mongoose.Schema.Types.ObjectId],
+			default: [],
+			ref: 'Event',
+		},
+		favoriteTrainers: {
+			type: [mongoose.Schema.Types.ObjectId],
+			default: [],
+			ref: 'Trainers',
+		},
+		image: {
+			type: String,
+			default: '',
+		},
+	})
 );
 
-const validateTrainee = (trainer) => {
-  const schema = Joi.object({
-    firstName: Joi.string()
-      .min(1)
-      .required(),
-    lastName: Joi.string()
-      .min(2)
-      .required(),
-    email: Joi.string()
-      .email({ minDomainSegments: 2 })
-      .required(),
-    password: Joi.string()
-      .min(6)
-      .max(13)
-      .required(),
-    registeredEvents: Joi.array().default([]),
-    favoriteTrainers: Joi.array().default([]),
-  });
-  return schema.validateTrainee(trainer);
+const validateTrainee = trainee => {
+	const schema = Joi.object({
+		firstName: Joi.string().min(2).required(),
+		lastName: Joi.string().min(2).required(),
+		email: Joi.string().email({ minDomainSegments: 2 }).required(),
+		registeredEvents: Joi.array().default([]),
+		favoriteTrainers: Joi.array().default([]),
+		image: Joi.string().min(0),
+	});
+	return schema.validateTrainee(trainee);
 };
 
 exports.Trainee = Trainee;
--- server/routes/trainers.js
+++ server/routes/trainers.js
@@ -1,54 +1,60 @@
 // Express modules imports
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // Custom modules & API imports
-const { Trainer, validate } = require("../models/trainer");
+const { Trainee, validate } = require('../models/trainee');
 
 // Create Methods
-router.post("/", async (req, res) => {
-  const trainerData = req.body;
+router.post('/', async (req, res) => {
+	const traineeData = req.body;
 
-  //Check if the request is legal
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+	//Check if the request is legal
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-  let trainer = new Trainer(trainerData);
-  trainer = await trainer.save();
-  res.send(trainer);
+	let trainee = new Trainee(traineeData);
+	trainee = await trainee.save();
+	res.send(trainee);
 });
 
 // Read Methods // Checks if the handle the sameID before countinue
-router.get("/:id", async (req, res) => {
-  const trainer = await Trainer.findById(req.params.id).populate({
-    path: "events",
-    select: "_id category date hour city address participants maxParticipants",
-  });
-  if (!trainer)
-    return res.status(404).send("The trainer with the given ID was not found.");
-  res.send(trainer); // Check if to return trainer and user after finding by the id
+router.get('/:id', async (req, res) => {
+	const trainee = await Trainee.findById(req.params.id)
+		.populate({
+			// For thr registered events list
+			path: 'registeredEvents',
+			select: '_id category date hour city address',
+		})
+		.populate({
+			path: 'favoriteTrainers',
+			select: '_id firstName lastName image',
+		});
+	if (!trainee)
+		return res.status(404).send('The trainee with the given ID was not found.');
+	res.send(trainee);
 });
 
-router.put("/:id", async (req, res) => {
-  // Check the request
-  const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.put('/:id', async (req, res) => {
+	// Check the request
+	const { error } = validate(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
 
-  const trainer = await Trainer.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  if (!trainer)
-    return res.status(404).send("The event with the given ID was not found.");
-  res.send(trainer);
+	const trainee = await Trainee.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+	});
+	if (!trainee)
+		return res.status(404).send('The event with the given ID was not found.');
+	res.send(trainee);
 });
 
 // Handling delete method
-router.delete("/:id", async (req, res) => {
-  const trainer = await Trainer.findByIdAndRemove(req.params.id);
-  if (!trainer)
-    return res.status(404).send("The trainer with the given ID was not found.");
-  // Remove from fireBase too
-  res.send(trainer);
+router.delete('/:id', async (req, res) => {
+	const trainee = await Trainee.findByIdAndRemove(req.params.id);
+	if (!trainee)
+		return res.status(404).send('The trainee with the given ID was not found.');
+	// TODO: Remove from fireBase too
+	res.send(trainee);
 });
 
 module.exports = router;
