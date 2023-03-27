ec4cbdc3d61176d7c9e2adb7d23b289dd4fa81ef
Oded <odedatias8115@gmail.com>
2023-03-27 17:14:06 +0300

    Fixing the title in register event

--- Screens/TraineeScreens/RegisterEvent.js
+++ Screens/TraineeScreens/RegisterEvent.js
@@ -1,30 +1,30 @@
 // RN core components & API imports
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text } from 'react-native';
 
 // Custom components imports
-import RegisterEventForm from "../../Components/RegisterEventOutput/RegisterEventForm";
+import RegisterEventForm from '../../Components/RegisterEventOutput/RegisterEventForm';
 
 // RegisterEvent Component
 const RegisterEvent = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Accept Our Policies to register</Text>
-      <RegisterEventForm />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.heading}>Accept our policies to register</Text>
+			<RegisterEventForm />
+		</View>
+	);
 };
 
 // RegisterEvent StyleSheet
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  heading: {
-    fontSize: 20,
-    fontWeight: "400",
-    marginBottom: 20,
-  },
+	container: {
+		flex: 1,
+		padding: 20,
+	},
+	heading: {
+		fontSize: 20,
+		fontWeight: '400',
+		marginBottom: 20,
+	},
 });
 
 export default RegisterEvent;
