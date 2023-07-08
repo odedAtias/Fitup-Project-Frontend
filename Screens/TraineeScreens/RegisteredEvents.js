1c5e7e6ad295952fc8a34db39c9403ec86fcf6da
Oded <odedatias8115@gmail.com>
2023-07-08 18:16:18 +0300

    Adjusting the height of RegisteredEvents screen

--- Screens/TraineeScreens/RegisteredEvents.js
+++ Screens/TraineeScreens/RegisteredEvents.js
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
 		paddingBottom: '15%',
 	},
 	noEventsContainer: {
-		height: '90%',
+		height: '100%',
 		justifyContent: 'center',
 		alignItems: 'center',
 		paddingHorizontal: '10%',
