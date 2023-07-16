17b62d22bc58ba9ed7d55609c5e2208563b51ef8
Oded <odedatias8115@gmail.com>
2023-07-16 18:14:19 +0300

    Adding marker of favorite trainers to trainees

--- Components/TraineeSide/EventsOutput/EventItem.js
+++ Components/TraineeSide/EventsOutput/EventItem.js
@@ -1,3 +1,6 @@
+// Hooks imports
+import {useContext} from 'react';
+
 // RN core components & API imports
 import {Pressable, StyleSheet} from 'react-native';
 
@@ -8,11 +11,28 @@ import EventItemDetails from './EventItemDetails';
 // Constants
 import Colors from '../../../Constants/Colors';
 
+// Contexts imports
+import {TraineeContext} from '../../../store/TraineeContext';
+
 // EventItem component
 const EventItem = ({event, onPress}) => {
+	const tcx = useContext(TraineeContext);
+
+	const isFavoriteTrainer = tcx.favoriteTrainers.some(
+		trainer => trainer._id === event.trainer._id
+	);
+
 	const {_id, category, __v, ...rest} = event;
+
 	return (
-		<Pressable style={styles.container} onPress={onPress}>
+		<Pressable
+			style={
+				isFavoriteTrainer
+					? [styles.container, {backgroundColor: '#fff7a4'}]
+					: styles.container
+			}
+			onPress={onPress}
+		>
 			{/* Events trainer image container */}
 			<TrainerImage imageUrl={rest.trainer.image} />
 			{/* Events details container */}
@@ -26,7 +46,6 @@ const styles = StyleSheet.create({
 	container: {
 		flexDirection: 'row',
 		alignItems: 'center',
-		backgroundColor: Colors.Buttons.lightGray,
 		borderRadius: 10,
 		padding: 10,
 		// Adjust by the platforms
